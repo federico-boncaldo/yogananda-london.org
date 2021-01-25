21c48427e680f2d455bbdc6d1c1ab6efb089c739
Mike Langmayr <michael@langmayr.net>
2021-01-25 19:55:17 +0100

    Added design for front-page and secondary pages, added mobile menu

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,13 @@
-<header class="banner">
+<header class="banner {{ is_front_page() ? 'front-page':'' }}">
   <a class="brand" href="{{ home_url('/') }}">
-    <img alt="{{ get_bloginfo('name', 'display') }}" src="@asset('images/London_Centre_Logo_white.png')">
+    @if (is_front_page())
+      <img alt="{{ get_bloginfo('name', 'display') }}" src="@asset('images/London_Centre_Logo_white.png')">
+    @else
+    <img alt="{{ get_bloginfo('name', 'display') }}" src="@asset('images/SRF_London_Centre_Logo.png')">
+    @endif
   </a>
-  <nav class="nav-primary">
+
+  <nav class="nav-primary {{ is_front_page() ? 'front-page':'' }}">
     @if (has_nav_menu('primary_navigation'))
       {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
     @endif
