8136d4a9ebb149b5d6092787b7602cb9eef3555b
Mike Langmayr <michael@langmayr.net>
2021-02-01 15:20:04 +0100

    Use featured image for navaigation page header section

--- resources/views/template-navigation-page.blade.php
+++ resources/views/template-navigation-page.blade.php
@@ -7,7 +7,14 @@
 <div class="navigation-page">
   @section('content')
     @while(have_posts()) @php the_post() @endphp
-      @include('partials.page-header')
+      <div class="header-section">
+        {{ the_post_thumbnail() }}
+        <div class="overlay-title">
+          <p class="quote">{{ get_post(get_post_thumbnail_id())->post_excerpt }}</p>
+          <p class="quote-author">{{ get_post(get_post_thumbnail_id())->post_content }}</p>
+        </div>
+      </div>
+      
       @include('partials.content-page')
     @endwhile
   @endsection
