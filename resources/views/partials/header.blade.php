bc69c9df800c06ea8258c059b40c628c8d8b3be7
Mike Langmayr <michael@langmayr.net>
2021-07-23 17:59:28 +0200

    Updated styles, added social buttons

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -11,6 +11,14 @@
     @if (has_nav_menu('primary_navigation'))
       {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
     @endif
+
+    <a href="https://www.facebook.com/SRFLondon" target="_blank" class="social-button">
+      <img src="https://www.yoganandalondon.org/self-realisation-fellowship-img/facebook-icon.png" height="31" width="31">
+    </a>
+    <a href="https://www.instagram.com/srf_london_centre" target="_blank" class="social-button">
+      <img src="https://www.yoganandalondon.org/self-realisation-fellowship-img/instagram-icon.png" height="31" width="31">
+    </a>
+
     <button class="btn-blue donate-button"><a href="https://www.srf-london.org.uk/self-realisation-fellowship-img/Donations_to_the_London_Centre_SRF-Dec-2018.pdf" target="_blank">Donate</a></button>
   </nav>
 </header>
