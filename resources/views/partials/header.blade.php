d0449060f3caa2b407fcc84a0e0ffe0beff10705
Mike Langmayr <michael@langmayr.net>
2021-07-24 13:06:16 +0200

    added drop shadow to logo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,7 @@
 <header class="banner">
   <a class="brand" href="{{ home_url('/') }}">
-    @if (get_page_template_slug() == 'views/template-navigation-page.blade.php')
       <img alt="{{ get_bloginfo('name', 'display') }}" src="@asset('images/London_Centre_Logo_white.png')">
-    @else
-      <img alt="{{ get_bloginfo('name', 'display') }}" src="@asset('images/SRF_London_Centre_Logo.png')">
-    @endif
+      <!-- <img alt="{{ get_bloginfo('name', 'display') }}" src="@asset('images/SRF_London_Centre_Logo.png')"> -->
   </a>
 
   <nav class="nav-primary">
@@ -19,6 +16,6 @@
       <img src="https://www.yoganandalondon.org/self-realisation-fellowship-img/instagram-icon.png" height="31" width="31">
     </a>
 
-    <button class="btn-blue donate-button"><a href="https://www.srf-london.org.uk/self-realisation-fellowship-img/Donations_to_the_London_Centre_SRF-Dec-2018.pdf" target="_blank">Donate</a></button>
+    <button class="btn-blue donate-button"><a href="https://www.yoganandalondon.org/self-realisation-fellowship-img/Donations_to_the_London_Centre_SRF-Dec-2018.pdf" target="_blank">Donate</a></button>
   </nav>
 </header>
