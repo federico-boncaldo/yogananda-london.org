e109fd6828138614b943fa3f6848a586ec219f78
Mike Langmayr <michael@langmayr.net>
2021-07-30 19:39:59 +0200

    Improved footer alignment

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
-<footer class="content-info py-5">
-  <div class="container pt-5">
+<footer class="content-info">
+  <div class="container">
     <div class="row">
       <div class="col-md-4">
         @php dynamic_sidebar('sidebar-footer-1') @endphp
